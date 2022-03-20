52e06b6ab7d1f9260e3a2085130a548a6abd075a
houndthe <houndthe@protonmail.com>
2022-03-20 15:51:04 +0100

    Implement message channel and messaging

--- client.py
+++ client.py
@@ -1,11 +1,73 @@
+import json
 import logging
-from socket import socket, AF_INET, SOCK_STREAM
-from noise.connection import NoiseConnection
 import noise.constants as constants
+from socket import socket, AF_INET, SOCK_STREAM
+from typing import Tuple
+from noise.connection import NoiseConnection
+from noise.connection import Keypair
+from cryptography.hazmat.primitives.asymmetric import x25519
+from cryptography.hazmat.primitives import serialization
+
+
+class MessageChannel:
+    protocol_name = b"Noise_KK_25519_ChaChaPoly_SHA256"
+    # Using that by specification, maximum noise message is 64k
+    recv_buffersize = constants.MAX_MESSAGE_LEN
+    noise: NoiseConnection
+    soc: socket
+    user_key: x25519.X25519PrivateKey
+    server_key: x25519.X25519PublicKey
+
+    def __init__(
+        self,
+        soc: socket,
+        user_key: x25519.X25519PrivateKey,
+        server_key: x25519.X25519PublicKey,
+    ) -> None:
+        self.soc = soc
+        self.user_key = user_key
+        self.server_key = server_key
+        self.handshake()
+
+    def handshake(self) -> NoiseConnection:
+        noise = NoiseConnection.from_name(self.protocol_name)
+        noise.set_as_initiator()
+        noise.set_keypair_from_private_bytes(
+            Keypair.STATIC,
+            self.user_key.private_bytes(
+                serialization.Encoding.Raw,
+                serialization.PrivateFormat.Raw,
+                serialization.NoEncryption(),
+            ),
+        )
+        noise.set_keypair_from_public_bytes(
+            Keypair.REMOTE_STATIC,
+            self.server_key.public_bytes(
+                serialization.Encoding.Raw, serialization.PublicFormat.Raw
+            ),
+        )
+        noise.start_handshake()
+        message = noise.write_message()
+        self.soc.sendall(message)
+
+        received = self.soc.recv(self.recv_buffersize)
+        noise.read_message(received)
+
+        self.noise = noise
+
+    def send(self, data: bytes) -> None:
+        self.soc.sendall(self.noise.encrypt(data))
+
+    def receive(self) -> bytes:
+        data = self.soc.recv(self.recv_buffersize)
+        if data:
+            return self.noise.decrypt(data)
+
+        return None
 
 
 class RegisterChannel:
-    pattern_nn: bytes = b"Noise_NN_25519_ChaChaPoly_SHA256"
+    protocol_name: bytes = b"Noise_NN_25519_ChaChaPoly_SHA256"
     # Using that by specification, maximum noise message is 64k
     recv_buffersize = constants.MAX_MESSAGE_LEN
     noise: NoiseConnection
@@ -16,7 +78,7 @@ class RegisterChannel:
         self.handshake()
 
     def handshake(self) -> NoiseConnection:
-        noise = NoiseConnection.from_name(self.pattern_nn)
+        noise = NoiseConnection.from_name(self.protocol_name)
         # Set role in this connection as initiator
         noise.set_as_initiator()
         # Enter handshake mode
@@ -47,12 +109,28 @@ class RegisterChannel:
 
 
 class Client:
-    def register(self, host, port):
+    server_address: Tuple[str, int]
+    key_store: str
+    client_key: x25519.X25519PrivateKey
+    server_key: x25519.X25519PublicKey
+
+    def __init__(self, host, port) -> None:
+        self.server_address = (host, port)
+        self.client_key = x25519.X25519PrivateKey.generate()
+
+    def register(self, login):
+        """Register creates keypair for the client,
+        it then sends server the user login and its
+        public key. Server responds with its own public
+        key, so they can use these longterm static keys
+        for future authentication during messaging"""
+
         soc = socket(AF_INET, SOCK_STREAM)
-        soc.connect((host, port))
+        soc.connect(self.server_address)
 
         logging.debug("Registering...")
         soc.sendall(b"REGISTER\n")
+
         status = soc.recv(2048)
         if status != b"OK\n":
             logging.error("Register command failed")
@@ -60,16 +138,71 @@ class Client:
 
         channel = RegisterChannel(soc)
 
-        channel.send(b'{user: "pepa", shared_key: "kappa123"}')
+        pub_bytes = self.client_key.public_key().public_bytes(
+            serialization.Encoding.PEM,
+            serialization.PublicFormat.SubjectPublicKeyInfo,
+        )
+
+        register_data = {"login": login, "pubkey": pub_bytes.decode("utf8")}
+        logging.debug(f"Sending register data:\n{register_data}")
+        channel.send(json.dumps(register_data).encode("utf8"))
+
+        response = channel.receive()
+        logging.debug(f"Server responded with:\n{response}")
+
+        resp_js = json.loads(response)
+        server_pub = resp_js["pubkey"]
+        logging.debug(f"Got server public key:\n{server_pub}")
+
+        self.server_key = serialization.load_pem_public_key(server_pub.encode("utf8"))
+
+        logging.debug("Sending OK")
+        channel.send(b"OK\n")
+
         status = channel.receive()
-
-        logging.debug(f"Server responded with:\n{status}")
-
+        logging.debug(f"Got status {status} back")
         if status != b"OK\n":
-            logging.error("Register command failed")
+            logging.error("Registration failed")
             return
 
-        logging.debug("Register was successfull, closing connection")
+        logging.debug("Registration was successful, closing connection")
+        soc.close()
+
+    def message(self, login):
+        soc = socket(AF_INET, SOCK_STREAM)
+        soc.connect(self.server_address)
+
+        logging.debug("Messaging...")
+        soc.sendall(b"MESSAGE\n")
+
+        status = soc.recv(2048)
+        if status != b"OK\n":
+            logging.error("Message command failed")
+            return
+
+        soc.sendall(login.encode("utf8"))
+        status = soc.recv(2048)
+        if status != b"OK\n":
+            logging.error("Message command failed")
+            return
+
+        logging.debug("Creating Message channel")
+        channel = MessageChannel(soc, self.client_key, self.server_key)
+
+        # TODO add TPM
+
+        logging.debug("Starting interactive communication channel")
+        while True:
+            try:
+                message = input()
+                channel.send(message.encode("utf8"))
+                response = channel.receive()
+                print(f"Server: {response}")
+            except EOFError as ex:
+                channel.send(b"END\n")
+                break
+
+        logging.debug("Communication finished, closing connection")
         soc.close()
 
 
@@ -77,5 +210,6 @@ if __name__ == "__main__":
     # TODO let user set logging level in CLI
     logging.basicConfig(level=logging.DEBUG, format="%(levelname)s:%(message)s")
 
-    client = Client()
-    client.register("127.0.0.1", 65432)
+    client = Client("127.0.0.1", 65432)
+    client.register("xhajek10")
+    client.message("xhajek10")
--- server.py
+++ server.py
@@ -1,12 +1,80 @@
+import json
+from typing import Dict
+import noise.constants as constants
+import logging
 from socket import socket, AF_INET, SOCK_STREAM, SO_REUSEADDR, SOL_SOCKET
 from noise.connection import NoiseConnection
 from itertools import cycle
-import noise.constants as constants
-import logging
+from noise.connection import Keypair
+from cryptography.hazmat.primitives.asymmetric import x25519
+from cryptography.hazmat.primitives import serialization
+
+
+class MessageChannel:
+    protocol_name: bytes = b"Noise_KK_25519_ChaChaPoly_SHA256"
+    # Using that by specification, maximum noise message is 64k
+    recv_buffersize = constants.MAX_MESSAGE_LEN
+    noise: NoiseConnection
+    soc: socket
+    user_key: x25519.X25519PublicKey
+    server_key: x25519.X25519PrivateKey
+
+    def __init__(
+        self,
+        soc: socket,
+        user_key: x25519.X25519PublicKey,
+        server_key: x25519.X25519PrivateKey,
+    ) -> None:
+        self.soc = soc
+        self.user_key = user_key
+        self.server_key = server_key
+        self.handshake()
+
+    def handshake(self) -> NoiseConnection:
+        noise = NoiseConnection.from_name(self.protocol_name)
+        noise.set_as_responder()
+        noise.set_keypair_from_private_bytes(
+            Keypair.STATIC,
+            self.server_key.private_bytes(
+                serialization.Encoding.Raw,
+                serialization.PrivateFormat.Raw,
+                serialization.NoEncryption(),
+            ),
+        )
+        noise.set_keypair_from_public_bytes(
+            Keypair.REMOTE_STATIC,
+            self.user_key.public_bytes(
+                serialization.Encoding.Raw, serialization.PublicFormat.Raw
+            ),
+        )
+        # Enter handshake mode
+        noise.start_handshake()
+
+        for action in cycle(["receive", "send"]):
+            if noise.handshake_finished:
+                break
+            elif action == "receive":
+                data = self.soc.recv(self.recv_buffersize)
+                noise.read_message(data)
+            elif action == "send":
+                ciphertext = noise.write_message()
+                self.soc.sendall(ciphertext)
+
+        self.noise = noise
+
+    def send(self, data: bytes) -> None:
+        self.soc.sendall(self.noise.encrypt(data))
+
+    def receive(self) -> bytes:
+        data = self.soc.recv(self.recv_buffersize)
+        if data:
+            return self.noise.decrypt(data)
+
+        return None
 
 
 class RegisterChannel:
-    pattern_nn: bytes = b"Noise_NN_25519_ChaChaPoly_SHA256"
+    protocol_name: bytes = b"Noise_NN_25519_ChaChaPoly_SHA256"
     # Using that by specification, maximum noise message is 64k
     recv_buffersize = constants.MAX_MESSAGE_LEN
     noise: NoiseConnection
@@ -16,10 +84,8 @@ class RegisterChannel:
         self.soc = soc
         self.handshake()
 
-        # By now we should have noise connection setup
-
     def handshake(self) -> NoiseConnection:
-        noise = NoiseConnection.from_name(self.pattern_nn)
+        noise = NoiseConnection.from_name(self.protocol_name)
         # Set role in this connection as responder
         noise.set_as_responder()
         # Enter handshake mode
@@ -53,6 +119,11 @@ class Server:
     host: str = "127.0.0.1"
     port: int = 65432
     recv_buffer: int = 4096
+    server_key: x25519.X25519PrivateKey
+    users: Dict[str, x25519.X25519PublicKey] = {}
+
+    def __init__(self) -> None:
+        self.server_key = x25519.X25519PrivateKey.generate()
 
     def register_command(self, soc: socket):
         channel = RegisterChannel(soc)
@@ -60,10 +131,44 @@ class Server:
         register_data = channel.receive()
         logging.info(f"Received register data:\n{register_data}")
 
+        reg_js = json.loads(register_data)
+        self.users[reg_js["login"]] = serialization.load_pem_public_key(
+            reg_js["pubkey"].encode("utf8")
+        )
+
         # TODO add processing when we'll decide what the data should be
+
+        # Send back server public key
+        pub_bytes = self.server_key.public_key().public_bytes(
+            serialization.Encoding.PEM,
+            serialization.PublicFormat.SubjectPublicKeyInfo,
+        )
+
+        channel.send(json.dumps({"pubkey": pub_bytes.decode("utf8")}).encode("utf8"))
 
         # Confirm and finish connection
         channel.send(b"OK\n")
+
+    def message_command(self, soc: socket):
+        login = soc.recv(2048).decode("utf8")
+        if login not in self.users.keys():
+            logging.error(f"User {login} not registered")
+            soc.sendall(b"ERROR\n")
+            return
+
+        logging.debug(f"Found user {login}")
+
+        soc.sendall(b"OK\n")
+
+        logging.debug("Opening Message channel")
+        channel = MessageChannel(soc, self.users[login], self.server_key)
+
+        logging.debug("Echoing messages")
+        response = b""
+        while response != b"END\n":
+            response = channel.receive()
+            channel.send(response)
+            logging.debug(f"Received {response}")
 
     def handler(self, soc: socket):
         command = soc.recv(self.recv_buffer)
@@ -80,7 +185,7 @@ class Server:
             self.register_command(soc)
 
         if command == b"MESSAGE\n":
-            logging.error("Message command not yet implemented")
+            self.message_command(soc)
 
         logging.debug("Request finished, closing connection")
         soc.close()
